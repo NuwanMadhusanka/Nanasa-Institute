35746b5e9b32170e7ad8d519cf524147f8dd02b6
Nuwan <nuwanmadhusankarulz@gmail.com>
2020-06-21 12:22:19 +0530

    Initialize Application

---
+++ NanasaApp/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+
+const App: () => React$Node = () => {
+  return (
+    <Text>Nanasa App</Text>
+  );
+};
+
+
+
+export default App;
